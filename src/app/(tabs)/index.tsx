08c2a0fe698f3718deaf893b7f7c1ab8a554c13e
Shubham Kumar <Shubhamkumar2056@gmail.com>
2025-06-19 23:50:37 +0530

    Add MenuScreen and ProductListItem component for product display

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,43 +1,17 @@
-import { StyleSheet, Text, View, Image } from "react-native";
+import {   View } from "react-native";
 
-import EditScreenInfo from "@/src/components/EditScreenInfo";
 import Colors from "@/src/constants/Colors";
 // import { Text, View } from "@/src/components/Themed";
 import products from "@/assets/data/products";
+import { ProductListItem } from "@/src/components/ProductListItems";
 
-const product = products[0]
-export default function TabOneScreen() {
+
+export default function MenuScreen() {
   return (
-    <View style={styles.container}>
-      <Image source={{uri: product.image}} style={styles.image} />
-      <Text style={styles.title}>{product.name}</Text>
-      <Text style={styles.price}>${product.price}</Text>
-      <Text></Text>
+    <View >
+      <ProductListItem product={products[4]} />
+      <ProductListItem product={products[1]} />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    backgroundColor: 'white',
-    padding: 10,
-    borderRadius: 20,
-   
-  },
-  image: {
-    width: '100%',
-    aspectRatio: 1,
-
-
-  },
- 
-  title: {
-    fontSize: 18,
-    fontWeight: "600",
-    marginVertical: 10,
-  },
-  price: {
-    color: Colors.light.tint,
-    fontWeight: "bold"
-  }
-});
